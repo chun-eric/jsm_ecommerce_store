ac66883989509e797a23b0a0d4a7ea7ec9e8bfef
chun-eric <eric.chun88800888@gmail.com>
2022-06-20 13:31:37 +0900

    Made the footer , layout, footerbanner component

---
+++ ecommerce/components/Layout.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Head from "next/head";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>English PhraseCamp</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main
+        className="main-container
+    "
+      >
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
